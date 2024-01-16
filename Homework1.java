24b9a5d8ee3da1d11c6a6104cb51341524f31947
[sertar] <[tarasapiens@gmail.com]>
2024-01-17 01:37:31 +0300

    discription

---
+++ Homework1.java
@@ -0,0 +1,8 @@
+public class Homework1 {
+    public static void main(String[] args) {
+// возвращает количество символов в строке
+        String brand = "Gibson";
+        int result = brand.length();
+        System.out.println(result);
+    }
+}
