339ac735e9d2219f79e341c2a6feb36b461f94ff
[sertar] <[tarasapiens@gmail.com]>
2024-01-17 02:02:17 +0300

    discription

--- Homework1.java
+++ Homework1.java
@@ -4,5 +4,21 @@ public class Homework1 {
         String brand = "Gibson";
         int result = brand.length();
         System.out.println(result);
+
+// определяет пустоту строки. в данном случае отработало false - не пустая
+        String city = "Kazan";
+        boolean result_a = city.isEmpty();
+        System.out.println(result_a);
+
+        // Метод возвращает результат запроса, является ли эта строка «пустой»,
+        // не содержащих никаких символов, кроме пробелов. вернулся true
+
+        String channel = "     ";
+        boolean result_b = channel.isBlank();
+        System.out.println(result_b);
+
+
+
+
     }
 }
